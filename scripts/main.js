3169c7c6b9e70c841187033ac858f7a95907a94e
Nikita Zuev <nikita.zuev@arcadia.spb.ru>
2017-03-22 18:30:16 +0300

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+﻿function main(exports) {
+    const geometry = new exports.three.BoxGeometry( 1, 1, 1 );
+    const material = new exports.three.MeshBasicMaterial( { color: 0x00ff00} );//We make it green
+    const cube = new exports.three.Mesh( geometry, material );
+    //Add it to the screen
+    scene.add( cube );
+    cube.position.z = -3;//Shift the cube back so we can see it
+}
